b52bc78428147de96c48903a32f3b9c8c75d97f6
dangxiaojun <5678@qq.com>
2021-07-12 17:06:55 +0800

    update 纯ip访问得链接放行

--- main.go
+++ main.go
@@ -103,9 +103,14 @@ func serve(addr string) error {
 				return
 			}
 
-			domain, header, err := parseDomain(c, origDestPort)
+			domain, header, err, err2 := parseDomain(c, origDestPort)
+			domainDetail := domain
+			if err2 != nil {
+				log.Printf("从[%s > %s]读取数据失败: %v", c.RemoteAddr(), dest, err)
+				return
+			}
 			if err != nil {
-				log.Printf("从[%s]读取域名时发生错误: %v", c.RemoteAddr(), err)
+				domainDetail = fmt.Sprintf("没有读取到域名: %v", err)
 			}
 
 			r := acl.GetReport(domain, origDestAddr)
@@ -140,19 +145,19 @@ func serve(addr string) error {
 			remoteAddr := net.JoinHostPort(r.RedirectAddr, origDestPort)
 			rc, err := createRemote(remoteAddr, r.Mark)
 			if err != nil {
-				log.Printf("链接目标[%s] > [%s](%s)失败: %v", c.RemoteAddr(), remoteAddr, domain, err)
+				log.Printf("链接目标[%s] > [%s](%s)失败: %v", c.RemoteAddr(), remoteAddr, domainDetail, err)
 				return
 			}
 			defer rc.Close()
 
 			// relay
-			log.Printf("转发 [%s > %s > %s(%s)]", c.RemoteAddr(), rc.LocalAddr(), rc.RemoteAddr(), domain)
+			log.Printf("转发 [%s > %s > %s(%s)]", c.RemoteAddr(), rc.LocalAddr(), rc.RemoteAddr(), domainDetail)
 			_, err = io.Copy(rc, bytes.NewReader(header))
 			if err != nil && err != net.ErrClosed {
-				log.Printf("转发 [%s > %s > %s(%s)] header出现错误: %v", c.RemoteAddr(), rc.LocalAddr(), rc.RemoteAddr(), domain, err)
+				log.Printf("转发 [%s > %s > %s(%s)] header出现错误: %v", c.RemoteAddr(), rc.LocalAddr(), rc.RemoteAddr(), domainDetail, err)
 			}
 			if err := relay(rc.(*net.TCPConn), c); err != nil {
-				log.Printf("转发 [%s > %s > %s(%s)] 出现错误: %v", c.RemoteAddr(), rc.LocalAddr(), rc.RemoteAddr(), domain, err)
+				log.Printf("转发 [%s > %s > %s(%s)] 出现错误: %v", c.RemoteAddr(), rc.LocalAddr(), rc.RemoteAddr(), domainDetail, err)
 			}
 		}()
 	}
@@ -220,35 +225,35 @@ func readHeader(r *net.TCPConn) ([]byte, error) {
 	return buf[:n], nil
 }
 
-func parseDomain(r *net.TCPConn, port string) (string, []byte, error) {
+func parseDomain(r *net.TCPConn, port string) (string, []byte, error, error) {
 	switch port {
 	case "80":
 		return parseDomainHttp(r)
 	case "443":
 		return parseDomainHttps(r)
 	default:
-		return "", nil, nil
+		return "", nil, nil, nil
 	}
 }
 
-func parseDomainHttp(r *net.TCPConn) (string, []byte, error) {
+func parseDomainHttp(r *net.TCPConn) (string, []byte, error, error) {
 	header, err := readHeader(r)
 	if err != nil {
-		return "", nil, err
+		return "", nil, nil, err
 	}
 
 	// read all header
 	reg := regexp.MustCompile(`Host:.*\r\n`)
 	domains := reg.FindAllString(string(header), -1)
 	if domains == nil {
-		return "", header, fmt.Errorf("not found Host field")
+		return "", header, fmt.Errorf("not found Host field"), nil
 	}
 	domain := strings.TrimRight(strings.Split(domains[0], ":")[1], "\r\n")
 	domain = strings.TrimLeft(domain, " ")
-	return domain, header, nil
+	return domain, header, nil, nil
 }
 
-func parseDomainHttps(r *net.TCPConn) (string, []byte, error) {
+func parseDomainHttps(r *net.TCPConn) (string, []byte, error, error) {
 	/* 1   TLS_HANDSHAKE_CONTENT_TYPE
 	 * 1   TLS major version
 	 * 1   TLS minor version
@@ -276,7 +281,7 @@ func parseDomainHttps(r *net.TCPConn) (string, []byte, error) {
 	 */
 	header, err := readHeader(r)
 	if err != nil {
-		return "", nil, err
+		return "", nil, nil, err
 	}
 
 	const (
@@ -290,72 +295,72 @@ func parseDomainHttps(r *net.TCPConn) (string, []byte, error) {
 	length := len(header)
 	pos := 0
 	if length < TLSHEADERLEN+FIXEDLENGTHRECORDS {
-		return "", header, fmt.Errorf("tls header: not enough data")
+		return "", header, fmt.Errorf("tls header: not enough data"), nil
 	}
 	if int(header[0])&0x80 != 0 && int(header[2]) == 1 {
-		return "", header, fmt.Errorf("tls header: SSL 2.0, does not support SNI")
+		return "", header, fmt.Errorf("tls header: SSL 2.0, does not support SNI"), nil
 	}
 	if header[0] != TLSHANDSHAKECONTENTTYPE {
-		return "", header, fmt.Errorf("tls header: tls content type not 0x16")
+		return "", header, fmt.Errorf("tls header: tls content type not 0x16"), nil
 	}
 	if header[1] < 3 {
-		return "", header, fmt.Errorf("tls header: TLS major version < 3, does not support SNI")
+		return "", header, fmt.Errorf("tls header: TLS major version < 3, does not support SNI"), nil
 	}
 	recordLen := int(header[3])<<8 + int(header[4]) + TLSHEADERLEN
 	if length < recordLen {
-		return "", header, fmt.Errorf("tls header: not enough data2")
+		return "", header, fmt.Errorf("tls header: not enough data2"), nil
 	}
 	if header[TLSHEADERLEN] != TLSHANDSHAKETYPECLIENTHELLO {
-		return "", header, fmt.Errorf("tls header: invalid handshake type")
+		return "", header, fmt.Errorf("tls header: invalid handshake type"), nil
 	}
 
 	pos += TLSHEADERLEN + FIXEDLENGTHRECORDS
 	// skip session ID
 	if pos+1 > length || pos+1+int(header[pos]) > length {
-		return "", header, fmt.Errorf("tls header: not enough data3")
+		return "", header, fmt.Errorf("tls header: not enough data3"), nil
 	}
 	pos += 1 + int(header[pos])
 	// skip cipher suites
 	if pos+2 > length || (pos+2+(int(header[pos])<<8)+int(header[pos+1]) > length) {
-		return "", header, fmt.Errorf("tls header: not enough data4")
+		return "", header, fmt.Errorf("tls header: not enough data4"), nil
 	}
 	pos += 2 + (int(header[pos]) << 8) + int(header[pos+1])
 	// skip compression methods
 	if pos+1 > length || pos+1+int(header[pos]) > length {
-		return "", header, fmt.Errorf("tls header: not enough data5")
+		return "", header, fmt.Errorf("tls header: not enough data5"), nil
 	}
 	pos += 1 + int(header[pos])
 	// skip extension length
 	if pos+2 > length {
-		return "", header, fmt.Errorf("tls header: not enough data6")
+		return "", header, fmt.Errorf("tls header: not enough data6"), nil
 	}
 	pos += 2
 
 	// parse extension data
 	for {
 		if pos+4 > recordLen {
-			return "", header, fmt.Errorf("tls header: buffer more than one record, SNI still not found")
+			return "", header, fmt.Errorf("tls header: buffer more than one record, SNI still not found"), nil
 		}
 		if pos+4 > length {
-			return "", header, fmt.Errorf("tls header: not enough data7")
+			return "", header, fmt.Errorf("tls header: not enough data7"), nil
 		}
 		extDataLen := (int(header[pos+2]) << 8) + int(header[pos+3])
 		if int(header[pos]) == 0 && int(header[pos+1]) == 0 {
 			// server_name extension type
 			pos += 4
 			if pos+5 > length {
-				return "", header, fmt.Errorf("tls header: not server_name list header")
+				return "", header, fmt.Errorf("tls header: not server_name list header"), nil
 			}
 			serverNameLen := (int(header[pos+3]) << 8) + int(header[pos+4])
 			if pos+5+serverNameLen > length {
-				return "", header, fmt.Errorf("tls header: not server_name list header")
+				return "", header, fmt.Errorf("tls header: not server_name list header"), nil
 			}
 			// return server_name
 			if serverNameLen+1 > DOMAINLEN {
-				return "", header, fmt.Errorf("tls header: The domain name is too long")
+				return "", header, fmt.Errorf("tls header: The domain name is too long"), nil
 			}
 			serverName := header[pos+5 : pos+5+serverNameLen]
-			return string(serverName), header, nil
+			return string(serverName), header, nil, nil
 		} else {
 			// skip
 			pos += 4 + extDataLen
